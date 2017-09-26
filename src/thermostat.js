f670e90143d3b3b93531d4c509344d086c0997ea
William <wmcabangon@gmail.com>
2017-09-26 16:44:54 +0100

    maximum temperature is dictated by PSM status

--- src/thermostat.js
+++ src/thermostat.js
@@ -1,6 +1,8 @@
 'use strict';
 
 function Thermostat() {
+  this.MAX_LIMIT_PSM_ON = 25;
+  this.MAX_LIMIT_PSM_OFF = 32;
   this.MINIMUM_TEMPERATURE = 10;
   this.temperature = 20;
   this.powerSavingMode = true;
@@ -11,6 +13,9 @@ Thermostat.prototype.getCurrentTemperature = function() {
 };
 
 Thermostat.prototype.up = function () {
+  if(this.isMaximumTemperature()) {
+    return;
+  }
   this.temperature += 1;
 };
 
@@ -35,4 +40,11 @@ Thermostat.prototype.switchPowerSavingModeOff = function() {
 
 Thermostat.prototype.switchPowerSavingModeOn = function() {
   this.powerSavingMode = true;
+}
+
+Thermostat.prototype.isMaximumTemperature = function() {
+  if (this.isPowerSavingModeOn() === false) {
+    return this.temperature === this.MAX_LIMIT_PSM_OFF
+  }
+  return this.temperature === this.MAX_LIMIT_PSM_ON
 }
