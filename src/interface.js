26e461f123f9e106c5446c926ef708a1534a638f
Rory Hodgson <29726065+rorybot@users.noreply.github.com>
2017-09-27 16:06:58 +0100

    User can choose city and receive current temperature WOO ;)

--- src/interface.js
+++ src/interface.js
@@ -34,8 +34,13 @@ $(document).ready(function() {
     $('#temperature').attr('class', thermostat.energyUsage());
   }
 
-  $.get('http://api.openweathermap.org/data/2.5/weather?q=London,uk&APPID=a3d9eb01d4de82b9b8d0849ef604dbed&units=metric', function (data) {
+ $('#select-city').submit(function(event) {
+  event.preventDefault();
+  var city = $('#current-city').val();
+  $.get('http://api.openweathermap.org/data/2.5/weather?q=' + city + '&APPID=a3d9eb01d4de82b9b8d0849ef604dbed&units=metric', function (data) {
     $("#current-temperature").text(data.main.temp);
   })
+
+})
 
 });
