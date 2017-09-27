8ba0b1c5055c4cb40d5356fd840cd0d7ba582a99
Rory Hodgson <29726065+rorybot@users.noreply.github.com>
2017-09-27 16:19:18 +0100

    Refactored URL getter separate from Temperature Displayer

--- src/interface.js
+++ src/interface.js
@@ -34,13 +34,21 @@ $(document).ready(function() {
     $('#temperature').attr('class', thermostat.energyUsage());
   }
 
- $('#select-city').submit(function(event) {
-  event.preventDefault();
-  var city = $('#current-city').val();
-  $.get('http://api.openweathermap.org/data/2.5/weather?q=' + city + '&APPID=a3d9eb01d4de82b9b8d0849ef604dbed&units=metric', function (data) {
-    $("#current-temperature").text(data.main.temp);
-  })
+  function displayWeather(city) {
+    var url = 'http://api.openweathermap.org/data/2.5/weather?q=' + city;
+    var token = '&APPID=a3d9eb01d4de82b9b8d0849ef604dbed';
+    var units = '&units=metric';
+    $.get( url + token + units , function (data) {
+      $("#current-temperature").text(data.main.temp);
+    })
+  }
 
-})
+  displayWeather('London');
+
+  $('#select-city').submit(function(event) {
+    event.preventDefault();
+    var city = $('#current-city').val();
+    displayWeather(city);
+  })
 
 });
